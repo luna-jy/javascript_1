71e42f8a7f74e9d3086715e8fc66d15e8ac5452c
jooyoung <jooyoung.cairo@gmail.com>
2022-09-19 12:20:09 +0900

    javascript

---
+++ variables.js
@@ -0,0 +1,30 @@
+//변수 선언 키워드 :  var (구식..) / let[변수] const[상수]
+//1. short,int,long, float, double (java) / number (javascript)
+'use strict';
+let a = 5; //number , Number()
+console.log("a의 자료형 : "+typeof a )
+
+let b = 5.0; //number
+console.log("b의 자료형 : "+typeof b )
+
+let c = "test"; //string ,String()
+console.log('c의 자료형 : ${typeof c}')
+
+let d = "t"; //string
+let e = false; //boolean
+let f = null; //NULL
+let g; // Undefined (변수는 선언했지만, 값을 대입/할당하지 않은 상태)
+let h  = [1,2,3]; //Array
+let i = {
+          name: 'seon', 
+          job: 'tutor'
+        }   //object
+
+let j = function () {
+            console.log('hello javascript world');
+        } //function
+k = 100; 
+console.log(k);
+var kk= 1000;
+console.log(kk);
+        //일반적인 자바스크립트 타입 -> ES2015(ES6) 추가된  Symbol(), Class도 있음. .'use strict'; 사용시~
